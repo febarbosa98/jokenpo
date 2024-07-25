f4c783b92784bd1ef10b08f5ffe7249b16d22141
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-07-24 22:42:29 -0300

    adicionado o score

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+let score = 0
+let pcvitora = 0
+
 function playGame(userChoice) {
     const choices = ['Pedra', 'Papel', 'Tesoura'];
     const computerChoice = Math.floor(Math.random() * 3);
 
     let result = '';
-
+  
     if (userChoice === computerChoice) {
         result = 'Empate!';
     } else if ((userChoice === 0 && computerChoice === 2) || 
                (userChoice === 1 && computerChoice === 0) || 
                (userChoice === 2 && computerChoice === 1)) {
         result = 'Você ganhou!';
+        score++
     } else {
         result = 'Você perdeu!';
     }
 
-    console.log( `Você escolheu ${choices[userChoice]}, o computador escolheu ${choices[computerChoice]}. ${result}`);
+    // console.log( `Você escolheu ${choices[userChoice]}, o computador escolheu ${choices[computerChoice]}. ${result}`);
     document.getElementById('result').textContent = `Você escolheu ${choices[userChoice]}, o computador escolheu ${choices[computerChoice]}. ${result}`;
 
-    
-    
+    if(result = 'Você ganhou!' ){
+        document.getElementById('score').textContent = score 
+    }
 
 }
 
